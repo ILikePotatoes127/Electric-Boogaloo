cffcfb3d728d1b1c395fcb8fa0028cddedf9f930
Student BE106 <Student.BE106@chaffey.edu>
2024-11-21 13:50:46 -0800

    stuff

--- test/Assets/voltage_wall.cs
+++ test/Assets/voltage_wall.cs
@@ -5,7 +5,7 @@ public class voltage_wall : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
