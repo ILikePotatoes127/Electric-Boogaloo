5d0af3342c5d3afe702da4ee68d07fc000b57555
Student BE106 <Student.BE106@chaffey.edu>
2024-11-21 13:24:41 -0800

    smth

---
+++ test/Assets/voltage_wall.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class voltage_wall : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
