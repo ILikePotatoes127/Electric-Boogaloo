248abb68be74bba13af20ad439a8028b48069ab9
Student BE106 <Student.BE106@chaffey.edu>
2024-11-21 14:12:11 -0800

    transitions

--- test/Assets/Script stuff/SceneController.cs
+++ test/Assets/Script stuff/SceneController.cs
@@ -1,11 +1,14 @@
+using System.Collections;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements.Experimental;
+using static UnityEngine.GraphicsBuffer;
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController instance;
+    [SerializeField] Animator transitionAnim;
     private void Awake()
     {
         if (instance == null) 
@@ -20,7 +23,13 @@ public class SceneController : MonoBehaviour
     }
     public void NextLevel(string Target)
     {
-        
+        StartCoroutine(LoadLevel(Target));
+    }
+    IEnumerator LoadLevel(string Target)
+    {
+        transitionAnim.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(sceneName: Target);
+        transitionAnim.SetTrigger("End");
     }
 }
